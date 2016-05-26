fd11d57ad1f1c80c9fd60937e1b1613f33b70926
HeyZoos <jab347@pitt.edu>
2016-05-26 15:44:30 -0500

    smarter benchmark

--- bench/benchmark_timeseries.py
+++ bench/benchmark_timeseries.py
@@ -22,21 +22,21 @@ def main(year):
     # chicago
     timeseries_query(
         url=url,
-        query='agg=year&dataset_name=crimes_2001_to_present&obs_date__ge=' + year + '-1-1',
+        query='agg=year&obs_date__ge=' + year + '-1-1',
         geom=r'{"type"%3A"Feature"%2C"properties"%3A{}%2C"geometry"%3A{"type"%3A"Polygon"%2C"coordinates"%3A[[[-87.64313936058898%2C41.88330112135344]%2C[-87.64313936058898%2C41.893460648167355]%2C[-87.61472940270323%2C41.893460648167355]%2C[-87.61472940270323%2C41.88330112135344]%2C[-87.64313936058898%2C41.88330112135344]]]}}'
     )
 
     # new york
     timeseries_query(
         url=url,
-        query='agg=year&dataset_name=nypd_motor_vehicle_collisions&obs_date__ge=' + year + '-1-1',
+        query='agg=year&obs_date__ge=' + year + '-1-1',
         geom=r'{"type"%3A"Feature"%2C"properties"%3A{}%2C"geometry"%3A{"type"%3A"Polygon"%2C"coordinates"%3A[[[-74.13379666395485%2C40.6462615921222]%2C[-74.13379666395485%2C40.78782018739577]%2C[-73.83579251356423%2C40.78782018739577]%2C[-73.83579251356423%2C40.6462615921222]%2C[-74.13379666395485%2C40.6462615921222]]]}}'
     )
 
     # san francisco
     timeseries_query(
         url=url,
-        query='agg=week&dataset_name=sfpd_incidents_from_1_january_2003&obs_date__ge=' + year + '-1-1',
+        query='agg=week&obs_date__ge=' + year + '-1-1',
         geom='{"type"%3A"Feature"%2C"properties"%3A{}%2C"geometry"%3A{"type"%3A"Polygon"%2C"coordinates"%3A[[[-122.6364898122847%2C37.53586597792038]%2C[-122.6364898122847%2C37.95069515957716]%2C[-122.21626276150344%2C37.95069515957716]%2C[-122.21626276150344%2C37.53586597792038]%2C[-122.6364898122847%2C37.53586597792038]]]}}'
     )
 
