9cec2bc37e60f6543fe77fe4125cf361cc664e88
pbeckman <pbeckman@uchicago.edu>
2016-08-18 12:37:24 -0500

    updated input_test_metadata for new models

--- input_test_metadata.py
+++ input_test_metadata.py
@@ -1,158 +1,84 @@
 from plenario.sensor_network.sensor_models import NetworkMeta, NodeMeta, FeatureOfInterest, Sensor
-from plenario.database import session, Base, app_engine
+from plenario.database import session, Base, app_engine, session
 from geoalchemy2.elements import WKTElement
 import random
+from sqlalchemy import Table, MetaData
 
 aot = NetworkMeta(name='ArrayOfThings', info={
     "website": "aot.org",
-    "contact": "aot@chicago.org"
+    "contact": "admin@aot.org"
 })
 
-ios = NetworkMeta(name='InternetOfStuff', info={
-    "website": "ios.org",
-    "contact": "ios@seattle.org"
-})
+node = NodeMeta(id='00A',
+                sensor_network='ArrayOfThings',
+                location=WKTElement(
+                    'POINT(87.6298 41.8781)',
+                    srid=4326),
+                info={
+                    "height": {
+                        "value": 5,
+                        "unit": "meters"
+                    },
+                    "orientation": {
+                        "value": "NE",
+                        "unit": "Cardinal directions. One of N, NE, E, SE, S, SW, W, NW"
+                    }
+                })
 
-nodes = []
-for i in range(1, 31):
-    nodes.append(NodeMeta(id='ArrayOfThings' + str(i),
-                          sensorNetwork='ArrayOfThings',
-                          location=WKTElement(
-                              'POINT(' + str(random.randrange(-5, 5)) + ' ' + str(random.randrange(-5, 5)) + ')',
-                              srid=4326),
-                          info={
-                              "height": {
-                                  "value": random.randrange(3, 7),
-                                  "unit": "meters"
-                              },
-                              "orientation": {
-                                  "value": "NE",
-                                  "unit": "Cardinal directions. One of N, NE, E, SE, S, SW, W, NW"
-                              }
-                          }))
-
-iosnode = NodeMeta(id='InternetOfStuff1',
-                   sensorNetwork='InternetOfStuff',
-                   location=WKTElement(
-                       'POINT(' + str(random.randrange(-5, 5)) + ' ' + str(random.randrange(-5, 5)) + ')', srid=4326),
-                   info={
-                       "height": {
-                           "value": 5,
-                           "unit": "meters"
-                       },
-                       "orientation": {
-                           "value": "NE",
-                           "unit": "Cardinal directions. One of N, NE, E, SE, S, SW, W, NW"
-                       }
-                   })
-
-temp = FeatureOfInterest(name='temperature',
-                         observedProperties={'observedProperties': [
-                             {
-                                 "name": "temperature",
-                                 "type": "numeric",
-                                 "unit": "degrees Fahrenheit",
-                                 "description": "accurate within +- .5 degrees Fahrenheit"
-                             }
-                         ]})
-
-mag_field = FeatureOfInterest(name='magneticField',
-                              observedProperties={'observedProperties': [
-                                  {
-                                      "name": "X",
-                                      "type": "numeric",
-                                      "unit": "Gauss",
-                                      "description": "accurate within +- .002 Gauss"
-                                  },
-                                  {
-                                      "name": "Y",
-                                      "type": "numeric",
-                                      "unit": "Gauss",
-                                      "description": "accurate within +- .002 Gauss"
-                                  },
-                                  {
-                                      "name": "Z",
-                                      "type": "numeric",
-                                      "unit": "Gauss",
-                                      "description": "accurate within +- .002 Gauss"
-                                  }
-                              ]})
-
-atm = FeatureOfInterest(name='atmosphericPressure',
-                        observedProperties={'observedProperties': [
+gas = FeatureOfInterest(name='gasConcentration',
+                        observed_properties=[
                             {
-                                "name": "pressure",
+                                "name": "SO2",
                                 "type": "numeric",
-                                "unit": "atms",
-                                "description": "accurate"
-                            }
-                        ]})
-
-hum = FeatureOfInterest(name='humidity',
-                        observedProperties={'observedProperties': [
+                                "unit": "ppm",
+                                "description": "Sulfur dioxide concentration"
+                            },
                             {
-                                "name": "humidity",
+                                "name": "H2S",
                                 "type": "numeric",
-                                "unit": "relativistic speeds",
-                                "description": "wubalubadubdub"
-                            }
-                        ]})
+                                "unit": "ppm",
+                                "description": "Hydrogen sulfide concentration"
+                            },
+                            {
+                                "name": "O3",
+                                "type": "numeric",
+                                "unit": "ppm",
+                                "description": "Ozone concentration"
+                            },
+                            {
+                                "name": "NO2",
+                                "type": "numeric",
+                                "unit": "ppm",
+                                "description": "Nitrous oxide concentration"
+                            },
+                            {
+                                "name": "CO",
+                                "type": "numeric",
+                                "unit": "ppm",
+                                "description": "Carbon monoxide concentration"
+                            },
+                        ])
 
-tmp112 = Sensor(name='TMP112',
-                properties=['temperature.temperature', ],
-                info={"datasheet": "TMP112.datashe.et"})
+sensor1 = Sensor(name='Sulfur Dioxide', observed_properties=['gasConcentration.SO2'], info={"datasheet": "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasheets/chemsense/so2.pdf", "range": "0 to 20 ppm", "accuracy": "+-3% of reading"})
+sensor2 = Sensor(name='Hydrogen Sulfide', observed_properties=['gasConcentration.H2S'], info={"datasheet": "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasheets/chemsense/h2s.pdf", "range": "0 to 20 ppm", "accuracy": "+-3% of reading"})
+sensor3 = Sensor(name='Ozone', observed_properties=['gasConcentration.O3'], info={"datasheet": "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasheets/chemsense/o3.pdf", "range": "0 to 20 ppm", "accuracy": "+-3% of reading"})
+sensor4 = Sensor(name='Nitrous Oxide', observed_properties=['gasConcentration.NO2'], info={"datasheet": "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasheets/chemsense/no2.pdf", "range": "0 to 20 ppm", "accuracy": "+-3% of reading"})
+sensor5 = Sensor(name='Carbon Monoxide', observed_properties=['gasConcentration.CO'], info={"datasheet": "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasheets/chemsense/co.pdf", "range": "0 to 20 ppm", "accuracy": "+-3% of reading"})
 
-bmp340 = Sensor(name='BMP340',
-                properties=['temperature.temperature', 'humidity.humidity'],
-                info={"datasheet": "BMP340.datashe.et"})
 
-ubq120 = Sensor(name='UBQ120',
-                properties=['magneticField.X', 'magneticField.Y', 'magneticField.Z'],
-                info={"datasheet": "UBQ120.datashe.et"})
+# # init db
+# Base.metadata.create_all(app_engine)
 
-pre450 = Sensor(name='PRE450',
-                properties=['atmosphericPressure.pressure', ],
-                info={"datasheet": "PRE450.datashe.et"})
+# NEW
+# session.add(aot)
+# session.add(gas)
+session.add(node)
+node.sensors.append(sensor1)
+node.sensors.append(sensor2)
+node.sensors.append(sensor3)
+node.sensors.append(sensor4)
+node.sensors.append(sensor5)
 
-# init db
-Base.metadata.create_all(app_engine)
-
-# networks
-session.add(aot)
-session.add(ios)
 session.commit()
 
-# nodes
-for node in nodes:
-    session.add(node)
-session.add(iosnode)
-session.commit()
-
-# foi
-session.add(temp)
-session.add(mag_field)
-session.add(atm)
-session.add(hum)
-aot.featuresOfInterest.append(temp)
-aot.featuresOfInterest.append(mag_field)
-aot.featuresOfInterest.append(hum)
-ios.featuresOfInterest.append(atm)
-ios.featuresOfInterest.append(temp)
-session.commit()
-
-# sensors
-session.add(tmp112)
-session.add(bmp340)
-session.add(ubq120)
-temp.sensors.append(tmp112)
-temp.sensors.append(bmp340)
-mag_field.sensors.append(ubq120)
-hum.sensors.append(bmp340)
-atm.sensors.append(pre450)
-tmp112.sensorNetworks.append(aot)
-tmp112.sensorNetworks.append(ios)
-bmp340.sensorNetworks.append(aot)
-ubq120.sensorNetworks.append(aot)
-pre450.sensorNetworks.append(ios)
-session.commit()
 
